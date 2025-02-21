6cc4d840cc02d7dd4e325b996ace606e7381da69
Antonio Sachinski <as.sachinski@gmail.com>
2025-02-21 20:25:32 -0300

    Update index.js
    tornei o código mais legível

--- calculadora em módulos/index.js
+++ calculadora em módulos/index.js
@@ -1,10 +1,10 @@
 // CRIANDO UMA CALCULADORA DIVIDIDA EM MÓDULOS QUE RECEBE VALORES DO CONSOLE 
 
 // "chamando" os módulos
-var so = require("./so");
-var su = require("./su");
-var di = require("./di");
-var mu = require("./mu");
+var soma = require("./so");
+var subitracao = require("./su");
+var divisao = require("./di");
+var multiplicacao = require("./mu");
 
 // criando uma interface responsiva no console
 const readline = require("readline");
@@ -16,38 +16,38 @@ const rl = readline.createInterface({
 // RECEBENDO VALORES DO CONSOLE
 
 // criando uma função para receber o primeiro número
-rl.question("Digite um número? ", (n1) => {
-  n = Number(n1);
+rl.question("Digite um número? ", (valor1) => {
+  numero1 = Number(valor1);
   
   // criando uma função para receber o segundo número
-  rl.question("Digite um outro número? ", (n2) => {
-    nn = Number(n2);
+  rl.question("Digite um outro número? ", (valor2) => {
+    numero2 = Number(valor2);
 
     // criando uma função para escolher a operação
     console.log("1 - soma");
     console.log("2 - subtração");
     console.log("3 - divisão");
     console.log("4 - multiplicação");
-    rl.question("Qual operação você deseja efetuar? ", (op) => {
-      o = Number(op);
+    rl.question("Qual operação você deseja efetuar? ", (operacaoSelecionada) => {
+      operacao = Number(operacaoSelecionada);
 
       // calculando os possíveis resultados
-      switch (o) {
+      switch (operacao) {
         case 1:
           // atribuindo os parametro tratados pela função/ módulo a uma variavel
-          res = so(n, nn);
+          resultado = soma(numero1, numero2);
           break;
         case 2:
-          res = su(n, nn);
+          resultado = subitracao(numero1, numero2);
           break;
         case 3:
-          res = (di(n, nn)).toFixed(2);
+          resultado = (divisao(numero1, numero2)).toFixed(2);
           break;
         case 4:
-          res = mu(n, nn);
+          resultado = multiplicacao(numero1, numero2);
           break;
       }
-      console.log(`O resultado da operação é ${res}`);
+      console.log(`O resultado da operação é ${resultado}`);
     });
   });
 });
