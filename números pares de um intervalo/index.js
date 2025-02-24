ca8246baa38ffbe872cfe094574bf8e0bd8bf6bb
Antonio Sachinski <as.sachinski@gmail.com>
2025-02-23 23:43:23 -0300

    Update index.js
    melhorei a função, tornando as variáveis valorMenor e valorMaior externas as condições, assim usando apenas um for

--- números pares de um intervalo/index.js
+++ números pares de um intervalo/index.js
@@ -1,3 +1,21 @@
-var funcaoPares = require("./decobrirPares")
+var decobrirPares = function (valor1, valor2) {
+  if (valor1 == valor2) {
+    console.log('Os números são iguais');
+    return;
+  }
+  var valorMenor, valorMaior;
+  if (valor1 < valor2) {
+    valorMenor = valor1;
+    valorMaior = valor2;
+  } else {
+    valorMaior = valor1;
+    valorMenor = valor2;
+  }
+  for (valorMenor != valorMaior; valorMenor <= valorMaior; valorMenor++) {
+    if (valorMenor % 2 == 0) console.log(valorMenor);
+  }
+};
 
-console.log(funcaoPares(0, 10))
+decobrirPares(10, 1); 
+decobrirPares(1, 10); 
+decobrirPares(5, 5); 
