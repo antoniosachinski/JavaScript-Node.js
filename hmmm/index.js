18c95c9a9b12b348aaf6cc18f693d76d46bbeaf9
Antonio Sachinski <as.sachinski@gmail.com>
2025-07-30 15:16:15 -0300

    fix: retorna de fato a análise concatenando os pushs

--- hmmm/index.js
+++ hmmm/index.js
@@ -1,35 +1,36 @@
 var a = "Aa6aaaaaaaaaaaa!"
-const tipoSenha = []
-function verificaSenha(senha){
-    if(senha.length > 7){
-        if(/[A-Z]/.test(senha)){
-            tipoSenha.push = ("letra maiúscula,")
-            console.log(tipoSenha)
-            if(/[a-z]/.test(senha)){
-                tipoSenha.push = ("letra minúscula,")
-                console.log(tipoSenha)
-                if(/[0-9]/.test(senha)){
-                   tipoSenha.push = ("número,")
-                   console.log(tipoSenha)
-                   if(/[^A-Za-z0-9]/.test(senha)){
-                    tipoSenha.push = "especial"
-                    console.log(tipoSenha)
-                   }else{
-                    console.log("Sua senha tem pelo menos uma letra maiúscula, uma letra minúscula e um número, mas não possui caractere especial")
-                   }
-                }else{
-                    console.log("Sua senha tem pelo menos uma letra maiúscula e uma letra minúscula, mas não possui número e caractere especial")
-                }   
-            }else{
-                console.log("Sua senha tem pelo menos uma letra maiúscula, mas não possui letra minúscula, número e caractere especial")
-            }
-        }else{
-            console.log("Sua senha não possui nenhuma das características")
-        }
-    }else{
-        console.log("Muito curta")
+function verificaSenha(senha) {
+    const tipoSenha = []; // reinicia a cada execução
+
+    if (senha.length < 8) {
+        console.log("Muito curta");
+        return;
+    }
+
+    if (/[A-Z]/.test(senha)) {
+        tipoSenha.push("letra maiúscula");
+    }
+
+    if (/[a-z]/.test(senha)) {
+        tipoSenha.push("letra minúscula");
+    }
+
+    if (/[0-9]/.test(senha)) {
+        tipoSenha.push("número");
+    }
+
+    if (/[^A-Za-z0-9]/.test(senha)) {
+        tipoSenha.push("caractere especial");
+    }
+
+    if (tipoSenha.length === 4) {
+        console.log("Senha forte: contém " + tipoSenha.join(", "));
+    } else {
+        console.log("Senha fraca: contém " + tipoSenha.join(", ") + ".");
     }
 }
+
+
 
 /*/[A-Z]/.test(str)    // verifica se tem maiúscula
 /[a-z]/.test(str)    // minúscula
